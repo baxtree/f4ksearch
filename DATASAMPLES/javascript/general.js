62fd9978b7bb33d309cc7200a90e8306d8456841
baxtree <baxtree@163.com>
2013-06-12 21:41:33 +0100

    improve timezone and css

--- DATASAMPLES/javascript/general.js
+++ DATASAMPLES/javascript/general.js
@@ -2,26 +2,44 @@ $(document).ready(function () {
 	$("#tabs").tabs();
 		$("#startday").datepicker({ 
 			"dateFormat" : "dd/mm/yy",
-			"defaultDate": new Date(Date.parse("10/01/2010"))
+			"defaultDate": new Date(Date.parse("10/01/2010")),
+			"position" : {
+	 			"collision" : "flip"
+ 			}
+
 		});
 		$("#stopday").datepicker({ 
 			"dateFormat" : "dd/mm/yy",
-			"defaultDate": new Date(Date.parse("07/31/2013"))
+			"defaultDate": new Date(Date.parse("07/31/2013")),
+			"position" : {
+	 			"collision" : "flip"
+ 			}
+
 		});
 		$("#fulldate").datepicker({ 
-			"dateFormat" : "dd/mm/yy"
+			"dateFormat" : "dd/mm/yy",
+			"position" : {
+	 			"collision" : "flip"
+ 			}
+
 		});
 		$(".multiselect").multiselect({
  			"noneSelectedText" : "Select sites and cameras",
  			"classes" : "checkbox",
- 			"selectedList" : 3
+ 			"selectedList" : 3,
+ 			"position" : {
+	 			"collision" : "flip"
+ 			}
 		});
 		$(".singleselect").multiselect({
 			"multiple" : false,
 			"classes" : "radio",
 			"header" : "Hour : Min",
 			"noneSelectedText" : "Select a time",
-			"selectedList" : 3
+			"selectedList" : 3,
+			"position" : {
+				"collision" : "flip"
+			}
 
 		});
 		$(".filename:odd").addClass("odd");
